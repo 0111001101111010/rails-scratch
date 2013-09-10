4431d3b7d4eeb1245e41bc96cb4c282a8ee0d2d9
stanley zheng <szhen002@odu.edu>
2013-09-09 21:55:41 -0400

    added welcome index

---
+++ test_app/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+TestApp::Application.config.secret_key_base = '549ec7914679e0b4f15735ea68b634b2c0e861dfb02dc5870aa39caa1365a55e5c47c1db82f404ade1722000cf2fdb8d5c415262ff7b6e059d1e261bd8e70660'
